7f9cd3667a7d5c66b6a552c3330129251f0f4a51
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-21 14:54:01 +0100

    Adjust game speed and animation reset logic to reduce gap
    Reduced the game speed from 15 to 5 to slow down the animation. Adjusted the reset logic for the x and x2 variables in the animate function to reduce the gap between the repeating background images. Now, when x or x2 become less than -2400, they are reset to 2400 minus the game speed, ensuring a smoother transition and reducing the gap between the repeating images.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = (canvas.width = 800);
 const CANVAS_HEIGHT = (canvas.height = 700);
-let gameSpeed = 15;
+let gameSpeed = 5;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'layer-1.png';
@@ -22,9 +22,9 @@ function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.drawImage(backgroundLayer4, x, 0);
     ctx.drawImage(backgroundLayer4, x2, 0);
-    if (x < -2400) x = 2400;
+    if (x < -2400) x = 2400 - gameSpeed;
     else x -= gameSpeed;
-    if (x2 < -2400) x2 = 2400;
+    if (x2 < -2400) x2 = 2400 - gameSpeed;
     else x2 -= gameSpeed;
     requestAnimationFrame(animate);
 }
