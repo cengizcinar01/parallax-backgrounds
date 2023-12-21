0006408bc2cc66622e40b01d654f3c6203b7dba6
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-21 10:32:37 +0100

    Setup Parallax Background Layers in Canvas
    Enhanced the canvas configuration to include parallax background layers. Adjusted canvas dimensions to 800x700 for a broader visual area. Introduced a 'gameSpeed' variable for dynamic control of background movement. Added five image elements for different parallax layers, sourced from respective PNG files. This update paves the way for implementing a multi-layered parallax scrolling effect.

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = (canvas.width = 800);
+const CANVAS_HEIGHT = (canvas.height = 700);
+let gameSpeed = 5;
+
+const backgroundLayer1 = new Iamge();
+backgroundLayer1.src = 'layer-1.png';
+const backgroundLayer2 = new Iamge();
+backgroundLayer2.src = 'layer-2.png';
+const backgroundLayer3 = new Iamge();
+backgroundLayer3.src = 'layer-3.png';
+const backgroundLayer4 = new Iamge();
+backgroundLayer4.src = 'layer-4.png';
+const backgroundLayer5 = new Iamge();
+backgroundLayer5.src = 'layer-5.png';
